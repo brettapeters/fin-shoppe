f3388cabe4afb06b04d42a4977a00ace53eea2d5
Brett Peters <brett.peters1@gmail.com>
2016-12-29 21:30:48 -0700

    Remove require config in test, hard code it

--- test/fifo.js
+++ test/fifo.js
@@ -1,7 +1,14 @@
 var test = require('tape');
 var fs = require('fs');
+
 var fifo = require('../app/fifo');
-var { menu, baristas, workday } = require('../config/shoppe-details');
+var menu = [
+      { "type": "tea",      "brew_time": 3, "profit": 2 },
+      { "type": "latte",    "brew_time": 4, "profit": 3 },
+      { "type": "affogato", "brew_time": 7, "profit": 5 }
+    ],
+    baristas = [1, 2],
+    workday = { start: 0, end: 100 };
 
 var testInput = __dirname + '/sample-files/input.json';
 var testOutput = __dirname + '/sample-files/output_fifo.json';
