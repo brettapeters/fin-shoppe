560617ce6ca779946107b551ae2f102925d51f22
Brett Peters <brett.peters1@gmail.com>
2016-12-29 21:13:33 -0700

    Add fifo test

---
+++ test/fifo.js
@@ -0,0 +1,17 @@
+var test = require('tape');
+var fs = require('fs');
+var fifo = require('../app/fifo');
+var { menu, baristas, workday } = require('../config/shoppe-details');
+
+var testInput = __dirname + '/sample-files/input.json';
+var testOutput = __dirname + '/sample-files/output_fifo.json';
+var fulfill = fifo(menu, baristas, workday);
+
+test('fifo', function(t) {
+  var input = JSON.parse(fs.readFileSync(testInput));
+  var expected = JSON.parse(fs.readFileSync(testOutput));
+  var actual = fulfill(input);
+
+  t.deepEqual(expected, actual, 'Cool beans, it works');
+  t.end();
+});
